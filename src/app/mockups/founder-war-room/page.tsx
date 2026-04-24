ee49c4f87b0161afce687e23599526fa7471de2a
Elliot Sones <elliotsenos16@gmail.com>
2026-04-23 23:06:30 -0400

    2026-04-23 23:06 session checkpoint

---
+++ src/app/mockups/founder-war-room/page.tsx
@@ -0,0 +1,11 @@
+import FounderWarRoomMockup from "./FounderWarRoomMockup";
+
+export const metadata = {
+  title: "Founder War Room Mockup | Damasqas",
+  description:
+    "A bright animated landing page mockup for Damasqas, focused on startup founders booking a demo.",
+};
+
+export default function FounderWarRoomPage() {
+  return <FounderWarRoomMockup />;
+}
